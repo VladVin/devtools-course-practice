1904eb4d34eb8fd2c478e1e6b9e6c18f423b0b4e
alexey <alexeikokoshnikov@gmail.com>
2014-12-08 10:12:42 +0400

    add includes

---
+++ code/kokoshnikov-alexey/include/simplecalculator_application.h
@@ -0,0 +1,35 @@
+// Copyright 2014 Alexey Kokoshnikov
+
+#ifndef CODE_KOKOSHNIKOV_ALEXEY_INCLUDE_SIMPLECALCULATOR_APPLICATION_H_
+#define CODE_KOKOSHNIKOV_ALEXEY_INCLUDE_SIMPLECALCULATOR_APPLICATION_H_
+
+#include "include/simplecalculator.h"
+
+#include <stdint.h>
+#include <limits.h>
+#include <string.h>
+#include <math.h>
+#include <string>
+#include <cstdlib>
+
+#pragma pack(push, 1)
+typedef struct {
+    int arg1;
+    int arg2;
+    char operation;
+} Expression;
+#pragma pack(pop)
+
+class CalculatorApplication {
+ public:
+    CalculatorApplication();
+
+    std::string operator()(int argc, const char** argv);
+ private:
+    void help(const char* appname);
+    std::string parseArguments(int argc, const char** argv);
+    std::string Translate(std::string num1, std::string sys1, std::string sys2);
+    std::string message_;
+};
+
+#endif  // CODE_KOKOSHNIKOV_ALEXEY_INCLUDE_SIMPLECALCULATOR_APPLICATION_H_

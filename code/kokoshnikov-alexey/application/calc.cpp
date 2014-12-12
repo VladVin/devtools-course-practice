2719b00b7d1d89dc5db13234946510191c42dded
alexey <alexeikokoshnikov@gmail.com>
2014-12-13 01:05:40 +0400

    fix3

--- code/kokoshnikov-alexey/application/calc.cpp
+++ code/kokoshnikov-alexey/application/calc.cpp
@@ -8,6 +8,8 @@
 int main(int argc, const char** argv) {
     CalculatorApplication app;
     std::string output = app(argc, argv);
-    printf("App with parameters: %d, %s\n Out: %s", argc, argv[0], output);
+    printf("App with parameters: %d, %s\n Out: %s",
+                                     argc, argv[0],
+                                     output.c_str());
     return 0;
 }
